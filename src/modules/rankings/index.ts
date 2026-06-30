54a9084f32ca81c6a3edda88ce432747a557c581
Pedro Francisco <pdrfrncsco@gmail.com>
2026-06-30 22:32:13 +0100

    Eliminação de diretorios

--- src/modules/rankings/index.ts
+++
@@ -1,8 +0,0 @@
-// Rankings module public API
-export * from './components';
-export * from './hooks';
-export * from './pages';
-export * from './services';
-export * from './types';
-export * from './schemas';
-export * from './constants';
--- src/modules/stadiums/index.ts
+++
@@ -1,8 +0,0 @@
-// Stadiums module public API
-export * from './components';
-export * from './hooks';
-export * from './pages';
-export * from './services';
-export * from './types';
-export * from './schemas';
-export * from './constants';
